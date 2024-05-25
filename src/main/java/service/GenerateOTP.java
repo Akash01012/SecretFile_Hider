1ed425801f1cc1a130309d0ea52339590d280fb4
akash kumar <akash639673@gmail.com>
2024-05-25 12:35:19 +0530

    first commit

---
+++ src/main/java/service/GenerateOTP.java
@@ -0,0 +1,13 @@
+package service;
+
+import java.util.Random;
+
+public class GenerateOTP {
+    public static String getOTP(){
+
+        Random random =new Random();
+        return String.format("%4d",random.nextInt(10000));
+
+
+    }
+}
